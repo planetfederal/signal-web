d460ae5a75f73825b1a5bcfcb1f6a1b2490b5400
Wes Richardet <wes@tetriscodes.com>
2017-12-15 08:52:36 -0600

    Updating production url

--- config/production.js
+++ config/production.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://efc.boundlessgeo.com/api/';
-export const WS_URL = 'ws://efc.boundlessgeo.com:8086';
+export const API_URL = 'http://signal.boundlessgeo.com/api/';
+export const WS_URL = 'ws://signal.boundlessgeo.com:8086';
